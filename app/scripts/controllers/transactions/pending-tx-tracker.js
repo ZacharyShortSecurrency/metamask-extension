a04ec122b0c48b03d11bb692c6fc227c78ea7369
ZacharyShortSecurrency <zachary.short@securrency.com>
2022-09-11 22:53:34 -0500

    Replaced 'Welcome Back!' with 'Fathom Wallet'. Wallet now accepts xdc address from ethjs-query getTransactionReceipt response.

--- app/scripts/controllers/transactions/pending-tx-tracker.js
+++ app/scripts/controllers/transactions/pending-tx-tracker.js
@@ -1,6 +1,6 @@
 import EventEmitter from 'safe-event-emitter';
 import log from 'loglevel';
-import EthQuery from 'ethjs-query';
+import EthQuery from '../../../../ethjs-query';
 import { TRANSACTION_STATUSES } from '../../../../shared/constants/transaction';
 import { ERROR_SUBMITTING } from './tx-state-manager';
 
